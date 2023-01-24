8b773001793e2f1a19408e11f04d81a46ef0a974
Alex Horjakov <dia-zenyan@yandex.ru>
2023-01-24 16:28:56 +0300

    Адаптивная верстка, верстка попапа, написание скрипта

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 const popupElement = document.querySelector(".popup");
 const popupOpenButtonElement = document.querySelector(".profile__edit");
-const popupCloseButtonElement = popupElement.querySelector(".popup__button-close");
-const popupSaveButtonElement = popupElement.querySelector(".popup__button-save");
+const popupCloseButtonElement = document.querySelector(".popup__button-close");
+const popupSaveButtonElement = document.querySelector(".popup__button-save");
 const profileElement = document.querySelector(".profile");
 
 let profileNameElement = profileElement.querySelector(".profile__name");
@@ -11,34 +11,21 @@ let popupProfessionElement = document.querySelector(".popup__profession");
 
 
 const openPopup = function () {
-popupElement.classList.add("popup_opened");
-popupNameElement.value = profileNameElement.textContent;
-popupProfessionElement.value = profileProfessionElement.textContent;
+    popupElement.classList.add("popup_opened");
+    popupNameElement.value = profileNameElement.textContent;
+    popupProfessionElement.value = profileProfessionElement.textContent;
 }
-
 popupOpenButtonElement.addEventListener("click" , openPopup);
 
 const closePopup = function () {
     popupElement.classList.remove("popup_opened");
 }
-
 popupCloseButtonElement.addEventListener("click" , closePopup);
-popupSaveButtonElement.addEventListener("click" , closePopup);
 
-
-
-const closePopupByClickOnOverlay = function (event) {
-    if (event.target !== event.currentTarget) {
-        return;
-    }
-    closePopup();
-};
-popupElement.addEventListener("click", closePopupByClickOnOverlay);
-
-const OutText = function (evt) {
-    evt.preventDefault();
+const textWrapping = function (evt) {
+    evt.preventDefault ();
     profileNameElement.textContent = popupNameElement.value;
     profileProfessionElement.textContent = popupProfessionElement.value;
-};
-
-popupElement.addEventListener('submit', OutText);
+} 
+    popupElement.addEventListener("submit" , textWrapping);
+    popupSaveButtonElement.addEventListener("click" , closePopup);
