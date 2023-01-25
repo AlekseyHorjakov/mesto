f9ee629273230c3e6a11ea0e663ebc148706370b
Alex Horjakov <dia-zenyan@yandex.ru>
2023-01-25 13:09:34 +0300

    исправления после первого ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,12 @@
 const popupElement = document.querySelector(".popup");
 const popupOpenButtonElement = document.querySelector(".profile__edit");
 const popupCloseButtonElement = document.querySelector(".popup__button-close");
-const popupSaveButtonElement = document.querySelector(".popup__button-save");
 const profileElement = document.querySelector(".profile");
 
 let profileNameElement = profileElement.querySelector(".profile__name");
 let profileProfessionElement = profileElement.querySelector(".profile__profession");
-let popupNameElement = document.querySelector(".popup__name");
-let popupProfessionElement = document.querySelector(".popup__profession");
+let popupNameElement = document.querySelector(".popup__input_name");
+let popupProfessionElement = document.querySelector(".popup__input_profession");
 
 
 const openPopup = function () {
@@ -26,6 +25,6 @@ const textWrapping = function (evt) {
     evt.preventDefault ();
     profileNameElement.textContent = popupNameElement.value;
     profileProfessionElement.textContent = popupProfessionElement.value;
+    closePopup();
 } 
     popupElement.addEventListener("submit" , textWrapping);
-    popupSaveButtonElement.addEventListener("click" , closePopup);
